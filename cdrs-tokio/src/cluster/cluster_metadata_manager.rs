b470d59587fe979f96bf1dce3d1c1132da5050db
Cibin George <cibin@getblueshift.com>
2022-03-24 14:43:02 -0400

    Patching peers find for now

--- cdrs-tokio/src/cluster/cluster_metadata_manager.rs
+++ cdrs-tokio/src/cluster/cluster_metadata_manager.rs
@@ -340,7 +340,7 @@ impl<T: CdrsTransport + 'static, CM: ConnectionManager<T> + 'static> ClusterMeta
             contact_points,
             connection_pool_factory,
             did_initial_refresh: AtomicBool::new(false),
-            is_schema_v2: AtomicBool::new(true),
+            is_schema_v2: AtomicBool::new(false),
             session_context,
             node_distance_evaluator,
             version,
@@ -680,7 +680,7 @@ impl<T: CdrsTransport + 'static, CM: ConnectionManager<T> + 'static> ClusterMeta
 
     async fn query_peers(&self, transport: &T) -> Result<Option<Vec<Row>>> {
         let peers_v2_result = send_query(
-            "SELECT * FROM system.peers_v2",
+            "SELECT * FROM system.peers",
             transport,
             self.version,
             self.beta_protocol,
@@ -693,9 +693,9 @@ impl<T: CdrsTransport + 'static, CM: ConnectionManager<T> + 'static> ClusterMeta
                 additional_info: AdditionalErrorInfo::Invalid,
                 ..
             })) => {
-                self.is_schema_v2.store(false, Ordering::Relaxed);
+                self.is_schema_v2.store(true, Ordering::Relaxed);
                 send_query(
-                    "SELECT * FROM system.peers",
+                    "SELECT * FROM system.peers_v2",
                     transport,
                     self.version,
                     self.beta_protocol,
